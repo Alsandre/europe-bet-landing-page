27618468b53b0115efaad82df5fa03d213ba58ec
Aleksandre <gabeditebuli@gmail.com>
2023-04-23 18:50:42 +0400

    fix: first evet layout broken

--- src/components/prizeCard.tsx
+++ src/components/prizeCard.tsx
@@ -4,8 +4,10 @@ import { TPrizeCardProps } from '../types';
 export const PrizeCard: React.FC<TPrizeCardProps> = ({ icon, text }) => {
   return (
     <CardHorizontal>
-      <img src={icon} alt="" className="self-center" />
-      <span className="flex-1">{text}</span>
+      <div className="w-mobile m-auto text-[13px] flex gap-[8px] items-center pl-1">
+        <img src={icon} alt="" className="self-center" />
+        <span className="flex-1">{text}</span>
+      </div>
     </CardHorizontal>
   );
 };
