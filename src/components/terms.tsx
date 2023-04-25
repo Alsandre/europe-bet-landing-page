2ff4427bc3e9f22ba58c1b48fd4af48fd4f1a6d7
Aleksandre <gabeditebuli@gmail.com>
2023-04-25 08:32:57 +0400

    feat: add accordion functionality

--- src/components/terms.tsx
+++ src/components/terms.tsx
@@ -1,16 +1,25 @@
-import { DropDownIcon } from "../UI";
-import { TermsItem } from "./termsItem";
+import { Accordion, AccordionItem } from '@szhsin/react-accordion';
+import { TermsItem } from './termsItem';
+import { RULES } from '../constants';
 
 export const RulesAndTerms: React.FC = () => {
   function rulesClickHandler() {}
   return (
     <div className="w-mobile m-auto text-start mt-[55px]">
-      <h3 className="font-mtavruli font-medium text-16 pb-5">წესები და პირობები</h3>
-      <ul className="flex flex-col gap-[5px]">
-        <TermsItem text="როდის იწყება და რა ფორმატით გაიმართება აქცია" onClick={rulesClickHandler} />
-        <TermsItem text="როგორ მივიღო აქციაში მონაწილეობა?" onClick={rulesClickHandler} />
-        <TermsItem text="სხვადასხვა" onClick={rulesClickHandler} />
-      </ul>
+      <h3 className="font-mtavruli font-medium text-16 pb-5">
+        წესები და პირობები
+      </h3>
+      <Accordion>
+        {RULES.map(({ rule, description }) => (
+          <AccordionItem
+            header={({ state }) => {
+              return <TermsItem rotate={state.isEnter} text={rule} />;
+            }}
+          >
+            {description}
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
   );
 };
